e0ed6646e3586c956d760ec21e026c361b1fa0f0
nikunj_masrani_ss <nikunj.masrani@smartsensesolutions.com>
2022-11-30 11:13:29 +0530

    i18n support with get-text

--- app/helper/language_helper.py
+++ app/helper/language_helper.py
@@ -7,7 +7,9 @@ SUPPORTED_LANGUAGE = ["de", "en", "ru"]
 
 
 def language_translator(request: Request):
-    language = request.headers.get("accept-language", None)
+    language = request.headers.get("accept-language", DEFAULT_LANGUAGE)
+    if language not in SUPPORTED_LANGUAGE:
+        language = DEFAULT_LANGUAGE
     translate = gettext.translation(
         settings.localization_domain,
         localedir=settings.localization_dir,
